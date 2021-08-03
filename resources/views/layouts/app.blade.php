41ba939f97fce47e1eee8adc20523a81b35fe757
Mahfuzur Rahman Saber <saaberdev@gmail.com>
2021-08-03 08:46:27 +0600

    added blade layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+
+    {{-- Styles --}}
+    @stack('styles')
+</head>
+<body>
+  <div class="container-fluid ">
+      <div class="row">
+          {{-- Body Content--}}
+          @yield('content')
+      </div>
+  </div>
+  
+{{-- Scripts --}}
+@stack('scripts')
+
+</body>
+</html>
